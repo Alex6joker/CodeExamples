f50adfe4c2e39a35a65de8bd94327441eb710f5a
Alex6joker <38080538+Alex6joker@users.noreply.github.com>
2021-11-24 11:17:45 +0300

    Add files via upload

---
+++ CueWorker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Flac_with_CUE_to_Tak
+{
+    public class CueWorker
+    {
+        public bool WriteNewExtensionOnCueFile(String CueFilePath, String newExtension)
+        {
+            string str = string.Empty;
+            try
+            {
+                using (System.IO.StreamReader reader = new StreamReader(CueFilePath, System.Text.Encoding.UTF8, true))
+                {
+                    str = reader.ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            str = str.Replace(".wav", newExtension);
+            str = str.Replace(".tak", newExtension);
+
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(CueFilePath, false, System.Text.Encoding.UTF8))
+                {
+                    file.Write(str);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
